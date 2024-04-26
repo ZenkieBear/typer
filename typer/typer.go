994ace3f0b1be4958e16d278386dadfdef81f882
Zenkie Bear <zq@zenkie.cn>
2024-04-26 09:52:04 +0800

    perf: Exposed properties of structure Typer

--- typer/typer.go
+++ typer/typer.go
@@ -6,27 +6,39 @@ import (
 	"time"
 )
 
+// As same as fmt.Print
 type Printer = func(a ...any) (n int, err error)
 
 type Typer struct {
-	floatRange int
-	base       int
-	printer    Printer
+	// Minimum time to wait before each character input (millisecond).
+	Base int
+	// The interval of extra waiting time (millisecond).
+	// It makes the output appear more vivid.
+	FloatRange int
+	// The time to wait when encountering spaces (millisecond).
+	// Gap feels like thinking time before output the next word.
+	Gap int
+
+	// The function to print characters
+	Printer Printer
 }
 
-var DefaultTyper Typer = Typer{
-	floatRange: 200,
-	base:       10,
-	printer:    fmt.Print,
+func New(printer Printer, base, floatRange, gap int) *Typer {
+	return &Typer{
+		Printer:    printer,
+		Base:       base,
+		FloatRange: floatRange,
+		Gap:        gap,
+	}
 }
 
 func (t Typer) Print(message string) {
 	for _, c := range message {
-		delay := rand.Intn(t.floatRange) + t.base
+		delay := rand.Intn(t.FloatRange) + t.Base
 
 		time.Sleep(time.Duration(delay) * time.Millisecond)
 
-		t.printer(string(c))
+		t.Printer(string(c))
 
 		if string(c) == " " {
 			time.Sleep(200 * time.Millisecond)
@@ -36,6 +48,13 @@ func (t Typer) Print(message string) {
 
 func (t Typer) Println(message string) {
 	t.Print(fmt.Sprintf("%s\n", message))
+}
+
+var DefaultTyper Typer = Typer{
+	FloatRange: 200,
+	Base:       10,
+	Gap:        200,
+	Printer:    fmt.Print,
 }
 
 func Print(message string) {
